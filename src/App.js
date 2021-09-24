a552e33e5da76bc30e8040bfb738d0861e4829cb
danielrodrigues2019 <danielrodrigues129@hotmail.com>
2021-09-24 15:17:16 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import TabelaIMC from './componentes/TabelaIMC';
+import Peso from './componentes/Peso';
+import Altura from './componentes/Altura';
+import CalcularIMC from './componentes/CalcularIMC';
+import ResultadoIMC from './componentes/ResultadoIMC';
+
+export default function App() {
+
+  const [peso, setPeso] = useState(0)
+  const [altura, setAltura] = useState(0)
+  const [resultado, setResultado] = useState(0)
+
+  return (
+    <>
+      <Peso p={peso} sp={setPeso} />
+      <Altura a={altura} sa={setAltura} />
+      <CalcularIMC p={peso} a={altura} sr={setResultado}/>
+      <ResultadoIMC r={resultado}/>
+      <TabelaIMC/>
+    </>
+  )
+}
